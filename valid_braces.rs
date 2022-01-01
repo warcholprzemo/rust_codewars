95959c0e47de938b5a0fc63bd83c1ce176a50784
Przemysław Warchoł <przemyslawwarchol@scurri.com>
2022-01-01 15:43:39 +0100

    Update comments

--- valid_braces.rs
+++ valid_braces.rs
@@ -1,5 +1,6 @@
 /*
-All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+All input strings will be nonempty, and will only consist of
+parentheses, brackets and curly braces: ()[]{}.
 What is considered Valid?
 
 A string of braces is considered valid if all braces are matched with the correct brace.
